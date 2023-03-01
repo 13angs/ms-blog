b1b09db73ab8100c20bc24ab198daf2f5eb96024
13angs <13angz.um@gmail.com>
2023-03-01 10:17:41 +0000

    Setup nx-dotnet/core

---
+++ libs/generated/blog-sv-types/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/generated-blog-sv-types';
---
+++ libs/generated/blog-sv-types/src/lib/generated-blog-sv-types.spec.ts
@@ -0,0 +1,7 @@
+import { generatedBlogSvTypes } from './generated-blog-sv-types';
+
+describe('generatedBlogSvTypes', () => {
+  it('should work', () => {
+    expect(generatedBlogSvTypes()).toEqual('generated-blog-sv-types');
+  });
+});
