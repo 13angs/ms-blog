10d2d4690d8ff381fc23d184c0d5681ab9b2af7d
13angs <13angz.um@gmail.com>
2023-03-01 14:30:20 +0000

    blog-sv: add error response exception

--- apps/blog-sv/Program.cs
+++ apps/blog-sv/Program.cs
@@ -1,3 +1,5 @@
+using Api.Common.exceptions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,7 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+app.UseResponseExceptionHandler();
 
 app.UseAuthorization();
 
