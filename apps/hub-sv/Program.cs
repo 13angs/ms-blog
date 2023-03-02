e2e3c9fbe3095b3c0a1bdfa2a664502f84a49983
13angs <13angz.um@gmail.com>
2023-03-02 03:37:45 +0000

    hub-sv: subscribe from the MessageCollector

--- apps/hub-sv/Program.cs
+++ apps/hub-sv/Program.cs
@@ -1,3 +1,7 @@
+using hub_sv.BgServices;
+using Newtonsoft.Json.Serialization;
+using Simple.RabbitMQ;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IMessageSubscriber, MessageSubscriber>();
+builder.Services.AddHostedService<HubMessageCollector>();
+
+// configure controller to use Newtonsoft as a default serializer
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+            .Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.ContractResolver=new DefaultContractResolver();
+    }
+);
 
 var app = builder.Build();
 
@@ -16,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
