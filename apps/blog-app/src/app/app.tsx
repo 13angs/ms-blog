084368d057663bc824a7cafdae4a9456b397de42
13angs <13angz.um@gmail.com>
2023-03-03 06:35:31 +0000

    display real-time blog data using signalR client

--- apps/blog-app/src/app/app.tsx
+++ apps/blog-app/src/app/app.tsx
@@ -1,23 +1,51 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from 'react';
+import { HubConnectionBuilder, HttpTransportType, HubConnection } from '@microsoft/signalr';
 
+async function fetchBlog(url: string, setData: any) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    setData(data);
+  } catch (err) {
+    alert(err);
+  }
+}
 export function App() {
   const [blogData, setBlogData] = React.useState(null);
+  const [connection, setConnection] = React.useState<HubConnection | null>(null);
+  const url = `http://bs-blog.sample/api/v1/blog/blogs`;
+  const blogUrl = 'http://bs-blog.sample:80/hub/v1/blog'; //http:/bs-blog.sample
 
   React.useEffect(() => {
     // const url = process.env.NX_API_URL || '';
-    async function fetchBlog() {
-      try {
-        const res = await fetch(`http://bs-blog.sample/api/v1/blog/blogs`);
-        const data = await res.json();
-        setBlogData(data);
-      } catch (err) {
-        alert(err);
-      }
+
+
+    fetchBlog(url, setBlogData);
+  }, [url]);
+
+  React.useEffect(() => {
+
+    const newConnection = new HubConnectionBuilder()
+      .withUrl(blogUrl, {
+        skipNegotiation: true,
+        transport: HttpTransportType.WebSockets
+      })
+      .build();
+
+    newConnection.start().catch(err => {
+      console.log(err);
+    });
+    setConnection(newConnection);
+    newConnection.on("ReceiveAllMessage", async () => {
+      fetchBlog(url, setBlogData);
+    });
+
+    return () => {
+      newConnection.stop();
     }
 
-    fetchBlog();
-  }, [])
+  }, [url]);
 
   return (
     <div>
